20d591a501c862cea8e4c33930a84853ccfdc75a
Dave <R3P0FFXI@gmail.com>
2016-07-16 15:49:15 -0400

    Test

--- sql/custom/mob_family_system_custom.sql
+++ sql/custom/mob_family_system_custom.sql
@@ -6,4 +6,4 @@
 -- INSERT INTO `mob_family_system` VALUES (familyID,familyName,systemID,systemName,melee-size,speed,HP,MP,STR,DEX,VIT,AGI,INT,MND,CHR,ATT,DEF,ACC,EVA,Slash,Pierce,H2H,Impact,Fire,Ice,Wind,Earth,Lightning,Water,Light,Dark,Element);
 -- New info below this line Sorted by familyID starting at 4000.
 
-INSERT INTO `mob_family_system` VALUES (4000,'Tome',18,'Unclassified',0,40,100,120,3,3,3,3,3,3,3,3,3,3,3,1,1,1,1,1,1,1,1,1,1,1,1,0);
+INSERT INTO `mob_family_system` VALUES (4000,'Tome',18,'Unclassified',0,40,100,120,3,3,3,3,3,3,3,3,3,3,3,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0); -- Added new columns 0,0 at end of line to allow build
